230eee093af8a6c5f3ab8ba0d90ff6694458cdbc
Ubuntu <ubuntu@ip-172-31-34-204.eu-west-3.compute.internal>
2024-05-08 19:32:03 +0000

    updated the journale model

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [
+mode: 'production',
+publicDir: '/home/ubuntu/WebApps/portfolio/public',
+ plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
@@ -14,4 +16,27 @@ export default defineConfig({
         const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
         return pages[`./Pages/${name}.vue`]
       },
+	build: {
+	outDir: '/home/ubuntu/WebApps/portfolio/public/build',    
+rollupOptions: {
+      onwarn(warning, warn) {
+        // Ignore specific unresolved import warnings
+        if (warning.code === 'UNRESOLVED_IMPORT') {
+          console.warn('Ignored warning:', warning.message);  // Log to console if needed
+          return;  // Ignore this specific warning
+        }
+        // Handle all other warnings as usual
+        warn(warning);
+}}
+   
+  },
+
+ server: {
+    // Ensure this is only for development and not included in production
+    
+      host: '127.0.0.1',
+      port: 5173
+    
+  },
+
 });
